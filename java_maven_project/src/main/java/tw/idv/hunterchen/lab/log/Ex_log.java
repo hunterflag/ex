e99f2598fac32507bd4083ce4bf973a5754b9715
Hunter Flag <hunterflag@gmail.com>
2022-01-13 23:37:35 +0800

    update: log.level

--- java_maven_project/src/main/java/tw/idv/hunterchen/lab/log/Ex_log.java
+++ java_maven_project/src/main/java/tw/idv/hunterchen/lab/log/Ex_log.java
@@ -1,6 +1,7 @@
 package tw.idv.hunterchen.lab.log;
 
 import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,7 +16,7 @@ public class Ex_log {
 	 * 
 	 * 1.慣用以該類別名名稱當作 loggerName
 	 *  
-	 *	 
+	 * 預設等級是定義在執行環境的檔案: JRE安裝目錄/lib/logging.properties	 
 	 */
 	//XXX 1.LoggerName有前後代的順序關係?
 	//XXX 2.
@@ -50,11 +51,37 @@ public class Ex_log {
 	
 	public void bubbleUp() {
 		Logger logP = Logger.getLogger("t.p");
-		Logger logC = Logger.getLogger("t.p.c");
+		logP.setLevel(Level.ALL);
 		
-		logC.info("C bubbleUp To P");
-		logP.info("C bubbleUp To P");
+		ConsoleHandler consoleHandler = new ConsoleHandler();
+		consoleHandler.setLevel(Level.ALL);
+		logP.addHandler(consoleHandler);
+		
+//		ConsoleHandler[] handlers = (COnsoleHandler[]) logP.getHandlers();
+//		int length = handlers.length;
+//		System.out.println(length);
+//		System.out.println(consoleHandler.get);
+//		)[0].setLevel(Level.ALL);
 		//TODO 上傳是甚麼意思?
+		logP.severe("server級.嚴重資訊");
+		logP.warning("warning級.警示資訊");
+		logP.info("info級.一般資訊");
+		logP.config("config級.設定方面的資訊");
+		logP.fine("fine級.細微的資訊");
+		logP.finer("finer級.更細微的資訊");
+		logP.finest("finest級.最細微的資訊");
+
+		Logger logC = Logger.getLogger("t.p.c");
+		logC.setLevel(Level.FINE);
+		logC.severe("server級.嚴重資訊");
+		logC.warning("warning級.警示資訊");
+		logC.info("info級.一般資訊");
+		logC.config("config級.設定方面的資訊");
+		logC.fine("fine級.細微的資訊");
+		logC.finer("finer級.更細微的資訊");
+		logC.finest("finest級.最細微的資訊");
+		
+
 	}
 		
 	public static void main(String[] args) {
