85f6ca821994dd39d3784ca14be803a620816e23
Hunter Flag <hunterflag@gmail.com>
2022-01-22 00:42:45 +0800

    update: file sample

--- java_maven_project/src/main/java/tw/idv/hunterchen/lab/file/Ex_file.java
+++ java_maven_project/src/main/java/tw/idv/hunterchen/lab/file/Ex_file.java
@@ -6,6 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import tw.idv.hunterchen.utility.ShowTool;
 
 public class Ex_file {
 
@@ -17,33 +21,89 @@ public class Ex_file {
 	 */
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
-//		String fileName="/Users/chenhuanzhang/temp/exFile.txt";
+		/*
+//		File.createTempFile("AAA", ".ttt", new File("/Users/chenhuanzhang/temp/"));
+//		File.createTempFile("AAA", ".sss");
+		
+		File[] listRoots = File.listRoots();
+		for (File root : listRoots) {
+			ShowTool.showMessages(root.getName());
+		}
+		*/
+		
+		String pathName="/Users/chenhuanzhang/temp/exFile.txt";
+//		String pathName="/Users/chenhuanzhang/temp/";
 		
 //		String fileName="d:/temp/files/temp.txt";
 //		String folderName="d:/temp/files/temp.txt";
-		String folderName="d:/temp/files";
+//		String pathName="d:/temp/files";
+		File path = new File(pathName);
+
+		ShowTool.showMessages("pathName  ", String.valueOf(pathName), "原始名稱");
+		ShowTool.showMessages("getName() ", String.valueOf(path.getName()), "最後(右邊)１個pathName" );
+		ShowTool.showMessages("isAbsolute() ", String.valueOf(path.isAbsolute()), "原始名稱是否採絕對路徑");
 		
-		File folder = new File(folderName);
-		if (folder.isDirectory()) {
-			System.out.println(folder.getName() + " is a Directory!" );
-		}else{
-			System.out.println(folder.getName() + " is a file!" );
-			
-		};
-		String[] fileList = folder.list();
-		int fileCounter = fileList.length;
-		
-		if (fileCounter != 0) {
-			for (int i=0; i<fileCounter; i++) {
-				System.out.printf("%d:%s%n", i, fileList[i]);
-			}
-		}else {
-			System.out.println("no file");
-		}
+		ShowTool.showMessages("getAbsoluteFile() ", String.valueOf(path.getAbsoluteFile()), "系統絕對路徑檔案");
+		ShowTool.showMessages("getAbsolutePath() ", String.valueOf(path.getAbsolutePath()), "抽象絕對路徑檔案");
+		ShowTool.showMessages("getPath()  		 ", String.valueOf(path.getPath()), "路徑");
+
 		/*
-		File file = new File(fileName);
+		 */
+		ShowTool.showMessages("toURI()    ", String.valueOf(path.toURI()), "轉成URI路徑");
+		ShowTool.showMessages("toURL() 	  ", String.valueOf(path.toURI()), "轉成URL路徑");
+		ShowTool.showMessages("toString() ", String.valueOf(path.toString()), "路徑");
+		ShowTool.showMessages("toPath()   ", String.valueOf(path.toPath()), "路徑");
+
+		ShowTool.showMessages("getParent()     ", String.valueOf(path.getParent()), "父層路徑");
+		ShowTool.showMessages("getParentFile() ", String.valueOf(path.getParentFile()), "父層路徑檔案");
+		ShowTool.showMessages("getParentFile() ", String.valueOf(path.getParentFile()), "父層路徑檔案");
+		
+		/*
+		 */
+		ShowTool.showMessages("canExecute()", String.valueOf(path.canExecute()));
+		ShowTool.showMessages("canRead()   ", String.valueOf(path.canRead()));
+		ShowTool.showMessages("canWrite()  ", String.valueOf(path.canWrite()));
+		ShowTool.showMessages("exists()    ", String.valueOf(path.exists()));
+		ShowTool.showMessages("isHidden()  ", String.valueOf(path.isHidden()));
+		ShowTool.showMessages("lastModified()", String.valueOf(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(path.lastModified()))));
+		
+		/* 空間
+		 */
+		ShowTool.showMessages("getTotalSpace()  ", String.valueOf(path.getTotalSpace()));
+		ShowTool.showMessages("getUsableSpace() ", String.valueOf(path.getUsableSpace()));
+		
+		/*
+		 */
+		ShowTool.showMessages("hashCode() ", String.valueOf(path.hashCode()));
+		ShowTool.showMessages("length()parent. ", String.valueOf(path.length()));
+
+		
+		ShowTool.showMessages("mkdir()", String.valueOf(new File(path.getParent().toString()+"/newDir2/ddd").mkdir()));
+		ShowTool.showMessages("mkdirs()", String.valueOf(new File(path.getParent().toString()+"/newDir2/ddd").mkdirs()));
+		ShowTool.showMessages("mkdirs()", String.valueOf(new File(path.getParent().toString()+"/newDir2/ddd").mkdirs()));
+
+		
+		
+		if (path.isDirectory() && !(path.isFile())) {
+			System.out.println(path.getName() + " is a Directory!" );
+
+	
+//			XXX FileFilter fileFilter = new FileFilter();
+			String[] fileList = path.list();
+			int fileCounter = fileList.length;
+			
+			if (fileCounter != 0) {
+				for (int i=0; i<fileCounter; i++) {
+					System.out.printf("%d:%s%n", i, fileList[i]);
+				}
+			}else {
+				System.out.println("no file");
+			}
+		}else{
+			System.out.println(path.getName() + " is a file!" );
+			ShowTool.showMessages("space. ", String.valueOf(path.getAbsoluteFile()), "絕對路徑檔案");
 			try {
-				FileInputStream fileInputStream = new FileInputStream(file);
+				FileInputStream fileInputStream = new FileInputStream(path);
 				System.out.println("file real size (Bytes): "+fileInputStream.available());
 				//TODO read from file
 				
@@ -56,6 +116,11 @@ public class Ex_file {
 					fileInputStream.close();
 				}
 			}
+			
+		};
+
+		/*
+		File file = new File(fileName);
 		*/
 //		showFields(file);
 //		showMethods(file);
