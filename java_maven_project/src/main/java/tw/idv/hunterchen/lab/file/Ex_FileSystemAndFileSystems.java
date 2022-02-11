5d9560537d628ce85ec3b59b1d98b7a49f819b6c
Hunter Chen <hunterflag@gmail.com>
2022-02-11 16:43:45 +0800

    update FileSystem, Path, FileStore

--- java_maven_project/src/main/java/tw/idv/hunterchen/lab/file/Ex_FileSystemAndFileSystems.java
+++ java_maven_project/src/main/java/tw/idv/hunterchen/lab/file/Ex_FileSystemAndFileSystems.java
@@ -1,8 +1,12 @@
 package tw.idv.hunterchen.lab.file;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.FileStore;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.text.NumberFormat;
+import java.util.Iterator;
 
 import tw.idv.hunterchen.utility.DevTool;
 import tw.idv.hunterchen.utility.StringTool;
@@ -10,14 +14,18 @@ import tw.idv.hunterchen.utility.StringTool;
 public class Ex_FileSystemAndFileSystems {
 
 	public static void main(String[] args) throws URISyntaxException{
-		String divider = StringTool.genDivider(10);
-		//使用 FileSystems 取得 FileSystem 的方法
-		DevTool.showMessages(divider, "取得預設檔案系統", "getDefault()");
-		FileSystem fileSystem = FileSystems.getDefault();
+		FileSystem fileSystem;
+
+		DevTool.showMessages( StringTool.genDivider("|=>", 10)
+				, "使用 FileSystems 取得預設檔案系統"
+				, "getDefault()"
+				, String.valueOf(fileSystem = FileSystems.getDefault())
+				);
 		showFileSystem(fileSystem);
+		showFileStores(fileSystem);
 		
-		DevTool.showMessages("建立指定Path", fileSystem.getPath("d:", "temp", "files").toString());
-		DevTool.showMessages("取得所有磁碟機的根目錄 path", "getRootDirectories()", fileSystem.getRootDirectories().toString());
+//		DevTool.showMessages("建立指定Path", fileSystem.getPath("d:", "temp", "files").toString());
+//		DevTool.showMessages("取得所有磁碟機的根目錄 path", "getRootDirectories()", fileSystem.getRootDirectories().toString());
 
 		//		DevTool.showMessages(".", "取得當前路徑的檔案系統 file:");
 //		fileSystem = FileSystems.getFileSystem(new URI("."));
@@ -30,9 +38,37 @@ public class Ex_FileSystemAndFileSystems {
 	}
 	
 	private static void showFileSystem(FileSystem fileSystem) {
-		DevTool.showMessages("原始內容", fileSystem.toString());
-		DevTool.showMessages("分隔符號", fileSystem.getSeparator());
-		DevTool.showMessages("isOpen()", String.valueOf(fileSystem.isOpen()));
-		DevTool.showMessages("isReadOnly()", String.valueOf(fileSystem.isReadOnly()));
+		DevTool.showMessages("原始內容", 		"toString()",			fileSystem.toString());
+		DevTool.showMessages("分隔符號", 		"getSeparator()", 		fileSystem.getSeparator());
+		DevTool.showMessages("已開啟?",		"isOpen()", 			String.valueOf(fileSystem.isOpen()));
+		DevTool.showMessages("唯讀嗎?",		"isReadOnly()", 		String.valueOf(fileSystem.isReadOnly()));
+		DevTool.showMessages("取出所有根path",	"getRootDirectories()", String.valueOf(fileSystem.getRootDirectories()));
+	}
+	
+	private static void showFileStores(FileSystem fileSystem) {
+		Iterable<FileStore> fileStores = fileSystem.getFileStores();
+		
+		Iterator<FileStore> iterator = fileStores.iterator();
+		while (iterator.hasNext()) {
+			FileStore fileStore = iterator.next();
+			showFileStore(fileStore);
+		}
+	}
+	private static void showFileStore(FileStore fileStore) {
+		DevTool.showMessages(StringTool.genDivider("...", 100));
+		DevTool.showMessages("儲存器的名稱與代號", 	"toString()",	fileStore.toString());
+		DevTool.showMessages("儲存器的名稱", 		"name()", 		fileStore.name());
+		DevTool.showMessages("儲存器使用的檔案系統", "type()", 		fileStore.type());
+		
+		DevTool.showMessages("唯讀嗎?", "isReadOnly()", String.valueOf(fileStore.isReadOnly()));
+		
+		try {
+			DevTool.showMessages("總容量", 		"getTotalSpace()",			NumberFormat.getNumberInstance().format(fileStore.getTotalSpace()));
+			DevTool.showMessages("未配置容量",		"getUnallocatedSpace()",	NumberFormat.getNumberInstance().format(fileStore.getUnallocatedSpace()));
+			DevTool.showMessages("JVM可用容量",	"getUsableSpace()",			NumberFormat.getNumberInstance().format(fileStore.getUsableSpace()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- java_maven_project/src/main/java/tw/idv/hunterchen/lab/file/Ex_PathAndPaths.java
+++ java_maven_project/src/main/java/tw/idv/hunterchen/lab/file/Ex_PathAndPaths.java
@@ -1,6 +1,9 @@
 package tw.idv.hunterchen.lab.file;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -10,15 +13,37 @@ import tw.idv.hunterchen.utility.StringTool;
 public class Ex_PathAndPaths {
 
 	public static void main(String[] args) throws URISyntaxException {
-		String divider = StringTool.genDivider(10);
 		Path path = null;
 
-		DevTool.showMessages(divider, "使用 Paths 建立 Path .a.路徑字串法");
-//		path = Paths.get(".");
-		path = Paths.get("d:", "temp");
+		DevTool.showMessages(StringTool.genDivider("|=>", 10)
+				, "使用 Paths 建立 Path .a.路徑字串法"
+//				, String.valueOf(path = Paths.get("."))
+				, String.valueOf(path = Paths.get(".").toAbsolutePath())
+//				, String.valueOf(path = Paths.get("d:", "temp"))
+				);
 		showPath(path);
-		path = Paths.get("..", ".", "temp");
-		showPath(path);
+		showFiles(path);
+//		path.getFileSystem().getFileStores();
+
+//		DevTool.showMessages(StringTool.genDivider("...", 20));
+//		path = Paths.get("d:", "temp", "files", "sample.json");
+//		showPath(path);
+//		showFiles(path);
+//		
+//		DevTool.showMessages(StringTool.genDivider("...", 20));
+//		path = Paths.get("d:", "Program Files");
+//		showPath(path);
+//		showFiles(path);
+//		
+//		DevTool.showMessages(StringTool.genDivider("...", 20));
+//		path = Paths.get("d:", "hunter");
+//		showPath(path);
+//		showFiles(path);
+//		
+//		DevTool.showMessages(StringTool.genDivider("...", 20));
+//		path = Paths.get("c:", "Windows");
+//		showPath(path);
+//		showFiles(path);
 		
 //		DevTool.showMessages(divider, "使用 Paths 建立 Path .b.URI表示法");
 //		path = Paths.get(new URI("file://d:/temp/files"));
@@ -29,12 +54,32 @@ public class Ex_PathAndPaths {
 	}
 	
 	private static void showPath(Path path) {
+		DevTool.showMessages(StringTool.genDivider("---", 20));
 		DevTool.showMessages("原始 Path", path.toString());
-		DevTool.showMessages("自動搭配OS使用分隔符號"  , "toString()"	, path.toString());
-		DevTool.showMessages("是絕對路徑?"			  , "isAbsolute()"  , String.valueOf(path.isAbsolute()));
-		DevTool.showMessages("最後(右)的 Path"	  , "getFileName()" , String.valueOf(path.getFileName()));
-		DevTool.showMessages("從根目錄算起的 Path 層數", "getNameCount()", String.valueOf(path.getNameCount()));
-		DevTool.showMessages("取得上層 Path"	      , "getParent()"   , String.valueOf(path.getParent().getFileName()));
+		DevTool.showMessages("自動搭配OS使用分隔符號",	"toString()"	, path.toString());
+		DevTool.showMessages("是絕對路徑?",			"isAbsolute()"  , String.valueOf(path.isAbsolute()));
+		DevTool.showMessages("最後(右)的 Path", 		"getFileName()" , String.valueOf(path.getFileName()));
+		DevTool.showMessages("從根算到當前Path的層數",	"getNameCount()", String.valueOf(path.getNameCount()));
+		DevTool.showMessages("取得上層 Path",			"getParent()"   , String.valueOf(path.getParent().getFileName()));
+	}
+	
+	private static void showFiles(Path path) {
+		DevTool.showMessages(StringTool.genDivider("---", 20));
+		DevTool.showMessages("存在嗎?", 	"exists()",		String.valueOf((Files.exists(path, LinkOption.NOFOLLOW_LINKS))));
+		DevTool.showMessages("不存在嗎?", "notExists()",	String.valueOf((Files.notExists(path, LinkOption.NOFOLLOW_LINKS))));
+		DevTool.showMessages("資料夾嗎?", 	"isDirectory()", 	String.valueOf((Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS))));
+		DevTool.showMessages("是檔案嗎?", 	"isRegularFile()", 	String.valueOf((Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS))));
+		DevTool.showMessages("是link嗎?",	"isSymbolicLink()",	String.valueOf((Files.isSymbolicLink(path))));
+		DevTool.showMessages("可讀嗎?", 	"isReadable()", 	String.valueOf((Files.isReadable(path))));
+		DevTool.showMessages("可寫嗎?", 	"isWritable()", 	String.valueOf((Files.isWritable(path))));
+		DevTool.showMessages("可執行嗎?", "isExecutable()", 	String.valueOf((Files.isExecutable(path))));
+		try {
+			DevTool.showMessages("檔案大小(資料夾固定為4096)", String.valueOf(Files.size(path)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		DevTool.showMessages("可執行嗎?", "isExecutable()", 	String.valueOf((Files.getFileStore(path).)));
 		
 	}
 
