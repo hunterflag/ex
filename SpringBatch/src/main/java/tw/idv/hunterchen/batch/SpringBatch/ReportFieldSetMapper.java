b254580fca90d2643503cc6a0bd1fb92682d6ee6
Hunter Chen <hunterflag@gmail.com>
2022-02-16 17:23:40 +0800

    update

--- SpringBatch/src/main/java/tw/idv/hunterchen/batch/SpringBatch/ReportFieldSetMapper.java
+++ SpringBatch/src/main/java/tw/idv/hunterchen/batch/SpringBatch/ReportFieldSetMapper.java
@@ -10,27 +10,27 @@ import org.springframework.validation.BindException;
 import tw.idv.hunterchen.batch.SpringBatch.model.Report;
 
 public class ReportFieldSetMapper implements FieldSetMapper {
-	 
-	 private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-	 
-	 public Report mapFieldSet(FieldSet fieldSet) throws BindException {
-	  
-	  Report report = new Report();
-	  report.setId(fieldSet.readInt(0));
-	  report.setSales(fieldSet.readBigDecimal(1));
-	  report.setQty(fieldSet.readInt(2));
-	  report.setStaffName(fieldSet.readString(3));
-	  
-	  //fieldSet.readDate(4); //預設的 format yyyy-MM-dd
-	  String date = fieldSet.readString(4);
-	  try {
-	   report.setDate(dateFormat.parse(date));
-	  } catch (ParseException e) {
-	   e.printStackTrace();
-	  }
-	  
-	  return report;
-	  
-	 }
-	 
+
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+	public Report mapFieldSet(FieldSet fieldSet) throws BindException {
+
+		Report report = new Report();
+		report.setId(fieldSet.readInt(0));
+		report.setSales(fieldSet.readBigDecimal(1));
+		report.setQty(fieldSet.readInt(2));
+		report.setStaffName(fieldSet.readString(3));
+
+		// fieldSet.readDate(4); //預設的 format yyyy-MM-dd
+		String date = fieldSet.readString(4);
+		try {
+			report.setDate(dateFormat.parse(date));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return report;
+
 	}
+
+}
