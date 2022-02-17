da26ac6fd53f69e3e349866fa5732f68ea85659b
Hunter Chen <hunterflag@gmail.com>
2022-02-17 16:39:20 +0800

    update maven structure, add 2 samples

---
+++ SpringBatchdemoWar/src/main/java/tw/idv/hunterchen/lab/SpringBatchdemo/SpringBatchdemoApplication.java
@@ -0,0 +1,13 @@
+package tw.idv.hunterchen.lab.SpringBatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchdemoApplication.class, args);
+	}
+
+}
---
+++ SpringBatchdemoWar/src/test/java/tw/idv/hunterchen/lab/SpringBatchdemo/SpringBatchdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.hunterchen.lab.SpringBatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
