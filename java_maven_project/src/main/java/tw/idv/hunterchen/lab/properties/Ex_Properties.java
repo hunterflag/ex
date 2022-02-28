56b06b966e0b966a6778ebfd91035941eae4ba03
Hunter Flag <hunterflag@gmail.com>
2022-03-01 01:32:07 +0800

    just update

--- java_maven_project/src/main/java/tw/idv/hunterchen/lab/properties/Ex_Properties.java
+++ java_maven_project/src/main/java/tw/idv/hunterchen/lab/properties/Ex_Properties.java
@@ -3,18 +3,17 @@ package tw.idv.hunterchen.lab.properties;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
-import java.nio.file.Path;
 import java.util.Properties;
-import java.util.ResourceBundle;
+
+import tw.idv.hunterchen.utility.DevTool;
 
 public class Ex_Properties {
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
-		
-		//		File f = new File("Ex_Properties.properties");
+
+		// File f = new File("Ex_Properties.properties");
 //		File f = new File("\\tw\\idv\\hunterchen\\lab\\properties\\Ex_Properties.properties");
 //		File f = new File("/dev.source//test/src/main/resources/tw/idv/hunterchen/lab/properties/Ex_Properties.properties");
 //		File f = new File("/tw/idv/hunterchen/lab/properties/Ex_Properties.properties");
@@ -25,15 +24,32 @@ public class Ex_Properties {
 		 */
 //		File f = new File("d:/temp/Ex_Properties.properties");
 //		File f = new File("/temp/Ex_Properties.properties");
-		Path path = Paths.get(new URI("Ex_Properties.properties"));
-//		path.
-		File f = new File("D:/dev.source/lab/project/lab/java_maven_project/src/main/resources/tw/idv/hunterchen/lab/properties/Ex_Properties.properties");
+		// 執行時的 path (就是該專案目錄)
+		String currentFolder = Paths.get(".").toAbsolutePath().getParent().toString();
+		DevTool.showMessages(currentFolder);
+		// 該專案的classpath位置 (但部署時又該在哪裡？)
+		String classPath = currentFolder + "/target/classes/";
+		// 在執行時, properties檔的部署位置
+		String filePath = classPath + "/tw/idv/hunterchen/lab/properties/Ex_Properties.properties";
+		File file = new File(filePath);
+		
+		/* 在執行時, 原始 properties檔在專案的位置
+		 * 測試時, 原始檔同時在此處, 所以也可取得檔案, 容易被誤以為正確,  
+		 * 實則不然, 因為部署後原始環境並不在, 執行時就會找不到而出錯 
+		 */
+//		String filePath = "/src/main/resources/tw/idv/hunterchen/lab/properties/Ex_Properties.properties";
+//		File f = new File(currentFolder+"/"+filePath);
+//				"D:/dev.source/lab/project/lab/java_maven_project/src/main/resources/tw/idv/hunterchen/lab/properties/Ex_Properties.properties");
 
-		FileInputStream fis = new FileInputStream(f);
+		FileInputStream fis = new FileInputStream(file);
 //		BufferedReader bufferedReader = new BufferedReader(new FileReader());
 		Properties properties = new Properties();
+		// 設定 property.單個鍵值組
+		properties.put("keyName", "keyValue");
+		// 設定 property.從檔案輸入多個鍵值組
 		properties.load(fis);
-		
+
+		// 取出鍵值
 		System.out.println(properties.getProperty("demo.chinese"));
 	}
 
--- java_maven_project/src/test/java/tw/idv/hunterchen/utility/SftpClientTest.java
+++ java_maven_project/src/test/java/tw/idv/hunterchen/utility/SftpClientTest.java
@@ -4,8 +4,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SftpClientTest { 
-	private static final Logger logger = LoggerFactory.getLogger(SftpClientTest.class);
+//	private static final Logger log = LoggerFactory.getLogger(SftpClientTest.class);
 	static final String mDivider = StringTool.genDivider();
 
 	
