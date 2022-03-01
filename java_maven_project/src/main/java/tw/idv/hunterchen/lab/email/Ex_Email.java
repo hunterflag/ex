9dfdc6d6f8a296dc06437b70fd0eae430af39630
Hunter Chen <hunterflag@gmail.com>
2022-03-01 17:33:28 +0800

    update

--- java_maven_project/src/main/java/tw/idv/hunterchen/lab/email/Ex_Email.java
+++ java_maven_project/src/main/java/tw/idv/hunterchen/lab/email/Ex_Email.java
@@ -9,30 +9,52 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import lombok.extern.slf4j.Slf4j;
+import tw.idv.hunterchen.utility.DevTool;
 /*
  * http://tw.gitbook.net/javamail_api/javamail_api_sending_simple_email.html
+ * 主機資訊:
+ * host
+ * port
+ * 連線:
+ * username
+ * password
+ * ...
+ * 信件:
+ * from
+ * to, cc, bcc
+ * subject
+ * content
+ * MIME
+ * 
  */
 
+@Slf4j
 public class Ex_Email {
 	public static void main(String[] args) {
+		log.info("<=MBH=> start...");
 		// 收信人Recipient's email ID needs to be mentioned.
-		String to = "destinationemail@gmail.com";
+		String to = "developer@127.0.0.1";
 
 		// 發信人Sender's email ID needs to be mentioned
-		String from = "fromemail@gmail.com";
-		final String username = "manishaspatil";// change accordingly
-		final String password = "******";// change accordingly
+		String from = "developer@127.0.0.1";
+		final String username = "developer";// change accordingly
+		final String password = "developer";// change accordingly
 
 		// Assuming you are sending email through relay.jangosmtp.net
-		String host = "relay.jangosmtp.net";
-
-		Properties props = new Properties();
-
+		String host = "127.0.0.1";
+		
+		Properties props = System.getProperties();
+//		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", host);
 		props.put("mail.smtp.port", "25");
-
+		
+		DevTool.showSystem();
+		
+		
 		// Get the Session object.
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
