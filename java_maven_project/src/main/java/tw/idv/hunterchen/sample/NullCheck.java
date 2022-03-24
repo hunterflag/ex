cf2bd5bafc009dc805f82fe9ef17c9551d1fa4d6
Hunter Chen <hunterflag@gmail.com>
2022-03-24 17:31:33 +0800

    update

--- java_maven_project/src/main/java/tw/idv/hunterchen/sample/NullCheck.java
+++ java_maven_project/src/main/java/tw/idv/hunterchen/sample/NullCheck.java
@@ -5,7 +5,17 @@ import java.io.IOException;
 
 public class NullCheck {
 
-	public static void main(String[] args) {
+	public void seitch_null() {
+		String value=null;
+		switch (value) {		// 判斷式 不能為 null
+		case "str1": break;
+		case "str2": break;
+		default: break;	
+		}
+	}
+	
+	
+	public void string_null(String[] args) {
 		
 		String str=null;		
 		
@@ -15,13 +25,17 @@ public class NullCheck {
 			if (str==null || "".equals(str)) {
 //				throw NullPointerException;
 			}
-		} 
+//		} 
 //		catch(IOException ex) {
-			
-//		}
-		finally {
+		
+		} finally {
 			
 		}
+	}
+	
+	public static void main(String[] args) {
+		NullCheck nullCheck = new NullCheck();
+		nullCheck.seitch_null();
 		
 
 	}
