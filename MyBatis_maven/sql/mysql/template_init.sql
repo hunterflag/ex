0864d34a3522de69645853302d335d74eff21341
Hunter Chen <hunterflag@gmail.com>
2022-03-30 17:35:10 +0800

    update sql

---
+++ MyBatis_maven/sql/mysql/template_init.sql
@@ -0,0 +1,8 @@
+create table template (
+	id 				int 		primary key auto_Increment,
+	createdTime 	datetime	not null 	default now(),
+    modifiedTime 	timestamp 	not null
+);
+
+
+insert into template values;
--- MyBatis_maven/sql/mysql/test_init.sql
+++ MyBatis_maven/sql/mysql/test_init.sql
@@ -3,46 +3,10 @@
     2. 自訂欄位名稱: 全小寫, 以 _ 分隔 (搭配 myBatis)
 */
 use labDB;
--- 建立主表.xxx
-create table if not exists app_config (
-    key_name		varchar(100) 	not null 	unique,
-    key_value			varchar(100)	not null	default "",
+create table app_config (
+    name		varchar(100) 	not null,
+    password	varchar(100)	not null	default "0000",
 
-    id 				Integer 		primary key auto_increment,		
-    createdTime		datetime 		default now(),
-    modifiedTime	timestamp	default now()
+    no 			Integer 		primary key auto_increment,	
+    created		Date			not null 	default now()
 );
-
-drop table if exists app_config;
-
-commit;
-
-insert into app_config (key_name, key_value) values("key", "123");
-
-select * from app_config;
-
-update app_config 
-set key_value = "123"
-where key_name = "key"
-;
-
-
-select now();
-
--- 建立歷史表.xxx_history
--- 同時複製 資料表的結構、紀錄
-create table if not exists app_config_history 
-select * from app_config;	
-
-select * from app_config_history;
-
-delimiter ||;
-create trigger if not exists app_config_after_insert
-after insert on app_config
-for each row 
-begin
-	
-end
-||
-delimiter ; 
-;
