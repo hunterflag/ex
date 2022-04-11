216d23520c35eb9477ffc709c33f8187299db086
Hunter Chen <hunterflag@gmail.com>
2022-04-11 17:34:00 +0800

    update MyBatis

--- MyBatis_maven/src/main/java/tw/idv/hunterchen/lab/model/AbstractModel.java
+++ MyBatis_maven/src/main/java/tw/idv/hunterchen/lab/model/AbstractModel.java
@@ -3,10 +3,9 @@ package tw.idv.hunterchen.lab.model;
 import java.sql.Timestamp;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
-@Slf4j
+//@Slf4j
 public class AbstractModel {
 	private Integer serialNo;
 	
--- MyBatis_maven/src/main/java/tw/idv/hunterchen/lab/model/AppConfigModel.java
+++ MyBatis_maven/src/main/java/tw/idv/hunterchen/lab/model/AppConfigModel.java
@@ -1,9 +1,21 @@
 package tw.idv.hunterchen.lab.model;
 
-import lombok.Data;
+import java.sql.Timestamp;
 
-@Data
-public class AppConfigModel extends AbstractModel{
-	private String keyName;
-	private String keyValue;
+//import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Data
+@Setter
+@Getter
+public class AppConfigModel{
+	private String keyName;			// 鍵名
+	private String keyValue;		// 鍵值
+	
+	private Integer serialNo;		// 流水號
+	
+	private Timestamp createdTime;	// 建立時間
+	private Timestamp modifiedTime;	// 異動時間
+
 }
