0672ce87c0284f82a20759c9e44bdcd7e1a7af25
Hunter Chen <hunterflag@gmail.com>
2022-04-14 18:29:28 +0800

    springBoot properties

--- MyBatis_maven/src/main/java/tw/idv/hunterchen/lab/model/AppConfigHistory.java
+++ MyBatis_maven/src/main/java/tw/idv/hunterchen/lab/model/AppConfigHistory.java
@@ -2,19 +2,24 @@ package tw.idv.hunterchen.lab.model;
 
 import java.sql.Timestamp;
 
-//import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
-//@Data
-@Setter
-@Getter
+@Slf4j
+@Data	
+// =
+//@Setter
+//@Getter
+//@NoArgsConstructor
+//@EqualsAndHashCode
+//@ToString
 public class AppConfigHistory{
+	private Long serialNo;			// 流水號
+
 	private Long id;				// 識別碼
 	private String keyName;			// 鍵名
 	private String keyValue;		// 鍵值
 	
-	private Long serialNo;			// 流水號
 	private Timestamp createdTime;	// 建立時間
 	private Timestamp modifiedTime;	// 異動時間
 
--- springBoot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ springBoot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,37 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.system.SystemProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tw.idv.hunterchen.utility.DevTool;
 
+//@EnableConfigurationProperties(SystemProperties.class)
+//@PropertySource(
+//        value = {
+//                "classpath:application.yml"
+//        },
+//        factory = YamlPropertySourceFactory.class // <-- 指定為自訂的PropertySourceFactory類別
+//)
 @RestController
 @SpringBootApplication
+@PropertySource(value="classpath:application2.properties")
 public class DemoApplication {
+	@Value("${company.name}")
+	String companyName;
+	
 	@RequestMapping("/")
-	String index(@RequestParam("param") String param) {
+	String index(@RequestParam(value="param", defaultValue ="<H1>empty</H1>", required=false) String param) {
+		param = param==null ? "" : param ;
 		DevTool.showMessages("param", param);
-		return "Hello Spring Boot!";
+		DevTool.showMessages("company.name", companyName);
+		return "Hello Spring Boot!" + param + companyName;
 	}
 	
 	public static void main(String[] args) {
