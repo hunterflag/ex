74441784d918924af54a790ff35e950fddbc3234
Hunter Chen <hunterflag@gmail.com>
2023-03-10 17:58:15 +0800

    nothing

--- HunterSpringbootMybatis/src/main/java/tw/idv/hunterchen/springboot/main/Main.java
+++ HunterSpringbootMybatis/src/main/java/tw/idv/hunterchen/springboot/main/Main.java
@@ -2,8 +2,10 @@ package tw.idv.hunterchen.springboot.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class Main {
 
 	public static void main(String[] args) {
