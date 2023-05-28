8fcf6b7c040ac1939b133bd3f0e2ef40f494febd
Hunter Flag <hunterflag@gmail.com>
2023-05-29 00:59:47 +0800

    update

--- HunterLab/src/main/java/tw/idv/hunterchen/lab/datetime/Ex_Timestamp.java
+++ HunterLab/src/main/java/tw/idv/hunterchen/lab/datetime/Ex_Timestamp.java
@@ -8,20 +8,21 @@ import tw.idv.hunterchen.utility.DevTool;
 @Slf4j
 public class Ex_Timestamp {
 	public static void main(String[] args) throws InterruptedException  {
-		Long cts1, cts2;
-		cts1 = System.currentTimeMillis();
-		log.info("currentTimeMillis : {} ", cts1);
+		Long utcTimestamp1, utcTimestamp2;
+		
+		utcTimestamp1 = System.currentTimeMillis();
+		log.info("currentTimeMillis : {} ", utcTimestamp1);
 		Thread.sleep(1000);
-		cts2 = System.currentTimeMillis();
-		Long delta = cts2 - cts1; 
-		log.info("currentTimeMillis : {} ", cts2);
+		utcTimestamp2 = System.currentTimeMillis();
+		Long delta = utcTimestamp2 - utcTimestamp1; 
+		log.info("currentTimeMillis : {} ", utcTimestamp2);
 		log.info("delta = {} ", delta);
 		
-		Timestamp timestamp = new Timestamp(cts2);
+		Timestamp timestamp = new Timestamp(utcTimestamp2);
 		log.info("timestamp : {} ", timestamp.toString()); // 2022-07-25 14:30:04.448
 		log.info("timestamp : {} ", timestamp.getDate()); // 2022-07-25 14:30:04.448
 		
-		DevTool.showAllMembers(cts1);
+//		DevTool.showAllMembers(cts1);
 		
 	}
 }
