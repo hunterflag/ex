a25685bf58a2698b5220ba448cd9bdcfe246eae8
Hunter Flag <hunterflag@gmail.com>
2023-06-28 00:54:47 +0800

    <=MBH=>test

--- HunterLab/src/main/java/tw/idv/hunterchen/lab/jndi/Ex_Lookup.java
+++ HunterLab/src/main/java/tw/idv/hunterchen/lab/jndi/Ex_Lookup.java
@@ -38,35 +38,34 @@ import javax.naming.NamingException;
 import java.util.Hashtable;
 
 class Ex_Lookup {
-    public static void main(String[] args) {
-	// Check that user has supplied name of file to lookup
-	if (args.length != 1) {
-	    System.err.println("usage: java Lookup <filename>");
-	    System.exit(-1);
+	public static void main(String[] args) {
+		// Check that user has supplied name of file to lookup
+		if (args.length != 1) {
+			System.err.println("usage: java Lookup <filename>");
+			System.exit(-1);
+		}
+
+		String name = args[0];
+
+		// Identify service provider to use
+		Hashtable env = new Hashtable(11);
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
+
+		try {
+
+			// Create the initial context
+			Context ctx = new InitialContext(env);
+
+			// Look up an object
+			Object obj = ctx.lookup(name);
+
+			// Print it out
+			System.out.println(name + " is bound to: " + obj);
+
+			// Close the context when we're done
+			ctx.close();
+		} catch (NamingException e) {
+			System.err.println("Problem looking up " + name + ": " + e);
+		}
 	}
-
-	String name = args[0];
-
-	// Identify service provider to use
-	Hashtable env = new Hashtable(11);
-	env.put(Context.INITIAL_CONTEXT_FACTORY, 
-	    "com.sun.jndi.fscontext.RefFSContextFactory");
-
-	try {
-
-	    // Create the initial context
-	    Context ctx = new InitialContext(env);
-
-	    // Look up an object
-	    Object obj = ctx.lookup(name);
-
-	    // Print it out
-	    System.out.println(name + " is bound to: " + obj);
-	    
-	    // Close the context when we're done
-	    ctx.close();
-	} catch (NamingException e) {
-	    System.err.println("Problem looking up " + name + ": " + e);
-	}
-    }
 }
